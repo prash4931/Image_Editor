2b44e6e10f8b461ea647f0bec0e94fe53d6a3cf5
Prashant G <prashantgajre0@gmail.com>
2021-11-01 11:32:38 +0000

    Updated Manifest File

--- app/src/main/java/com/example/imageeditor/MainActivity.java
+++ app/src/main/java/com/example/imageeditor/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.imageeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +16,37 @@ public class MainActivity extends AppCompatActivity {
     private int REQUEST_CODE_PERMISSIONS = 101;
     private final String[] REQUIRED_PERMISSIONS = new String[] {
             "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.READ_EXTERNAL_STORAGE"
 
-    }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         img_cam = findViewById(R.id.imgCam);
+
+        if (allPermissionsGranted()) {
+            //Functional Code
+        } else {
+            ActivityCompat.requestPermissions(this,REQUIRED_PERMISSIONS,REQUEST_CODE_PERMISSIONS);
+        }
+
+        img_cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Raise a Toast
+            }
+        });
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission: REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
