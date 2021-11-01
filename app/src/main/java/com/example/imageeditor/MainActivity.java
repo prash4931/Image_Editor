1bfb66d9557d302def3e0e0afec427a57db358aa
Prashant G <prashantgajre0@gmail.com>
2021-11-01 11:19:52 +0000

    Updated Manifest File

--- app/src/main/java/com/example/imageeditor/MainActivity.java
+++ app/src/main/java/com/example/imageeditor/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.imageeditor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img_cam;
+
+    private int REQUEST_CODE_PERMISSIONS = 101;
+    private final String[] REQUIRED_PERMISSIONS = new String[] {
+            "android.permission.CAMERA",
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img_cam = findViewById(R.id.imgCam);
     }
 }
